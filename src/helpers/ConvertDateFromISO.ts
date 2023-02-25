300f6071a52f87c3cad0968864048c7f8188d23b
Luiz Fernando Cavalcanti <luizfcavalcanti@gmail.com>
2023-02-25 18:00:11 -0300

    Correction in Slice positions

--- src/helpers/ConvertDateFromISO.ts
+++ src/helpers/ConvertDateFromISO.ts
@@ -1,7 +1,7 @@
 export default function ConvertDateFromISO(dateString:string){
 
-    let day = Number(dateString.slice(9,10))
-    let month = Number(dateString.slice(6,7)) - 1
+    let day = Number(dateString.slice(8))
+    let month = Number(dateString.slice(5,7)) - 1
     let year = Number(dateString.slice(0,4))
 
     return new Date(year,month,day).toLocaleDateString()
