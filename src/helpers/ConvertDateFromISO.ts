723032a3601708472da68a7d62807693fae14347
Luiz Fernando Cavalcanti <luizfcavalcanti@gmail.com>
2023-02-28 17:21:24 -0300

    Simplifying the data convertion

--- src/helpers/ConvertDateFromISO.ts
+++ src/helpers/ConvertDateFromISO.ts
@@ -1,9 +1,5 @@
 export default function ConvertDateFromISO(dateString:string){
 
-    let day = Number(dateString.slice(8))
-    let month = Number(dateString.slice(5,7)) - 1
-    let year = Number(dateString.slice(0,4))
-
-    return new Date(year,month,day).toLocaleDateString()
+   return new Date(dateString+'T12:00:00').toLocaleDateString()
 
 }
