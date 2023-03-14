ada94e393c08366dff0fe1ae79b712d03614c9e6
Luiz Fernando Cavalcanti <luizfcavalcanti@gmail.com>
2023-03-14 17:01:02 -0300

    Aditional bottom border on the menu for the current page

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,8 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import {ReactComponent as Logo} from '/src/assets/logo.svg'
 import styles from './Menu.module.scss'
+import classnames from "classnames"
 
 export default function Menu() {
+
+    const currentPage = useLocation()
+
     const paginas = [
         {
             label: 'HOME',
@@ -34,7 +38,19 @@ export default function Menu() {
             </div>
             <nav className={styles.menu__nav}>
                 {paginas.map((pagina)=>(
-                    <Link key={pagina.label} to={pagina.to} id='pageLink' className={styles.menu__item}>{pagina.label}</Link>
+                    <Link
+                        key={pagina.label}
+                        to={pagina.to}
+                        id='pageLink'
+                        className={
+                            classnames(
+                                {
+                                    [styles.menu__item]:true,
+                                    [styles.menu__item__current]: pagina.to === currentPage.pathname
+                                }
+                            )
+                        }
+                    >{pagina.label}</Link>
                 ))}
             </nav>
         </header>
